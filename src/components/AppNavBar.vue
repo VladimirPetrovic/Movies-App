67e13ab3bbcb653490fb4b4d0dcaebc38a89b6f0
Vladimir Petrovic <petrovic.vladimir92@gmail.com>
2018-05-15 19:23:50 +0200

    Zadatak 1

---
+++ src/components/AppNavBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <router-link class="navbar-brand" :to="{name: 'home'}">Movies</router-link>
+        <div class="navbar-nav">
+          <router-link class="nav-item nav-link" :to="{name: 'movies'}">Posts</router-link>
+        </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
