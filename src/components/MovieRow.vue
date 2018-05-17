07c3e8c0c43035dbf2c962cbb69fd954c51b7a8e
Vladimir Petrovic <petrovic.vladimir92@gmail.com>
2018-05-17 20:02:53 +0200

    select/deselect all, pagination and filter

--- src/components/MovieRow.vue
+++ src/components/MovieRow.vue
@@ -1,16 +1,17 @@
 <template>
-  <div class="row" :class="{ 'highlighted': selected}">
+  <div class="row mb-2 mt-2" :class="{ 'highlighted': isSelected}">
     <div class="col-sm" v-text="movie.id"></div>
     <div class="col-sm" v-text="movie.title"></div>
     <div class="col-sm" v-text="movie.director"></div>
     <div class="col-sm" v-text="movie.releaseDate"></div>
     <div class="col-sm" v-text="movie.genre"></div>
+    <div class="col-sm" v-text="movie.duration"></div>
     <div class="col-sm">
       <b-button
         variant="primary"
         @click="onSelected"
       >
-        Select
+        Select / Deselect
       </b-button>
       </div>
   </div>
@@ -22,18 +23,22 @@ export default {
     movie: {
       type: Object,
       required: true
+    },
+    selectedMoviesIds: {
+      type: Array,
+      required: true,
+      default: () => []
     }
   },
-  data() {
-    return {
-      selected: false
+  computed: {
+    isSelected() {
+      return this.selectedMoviesIds.indexOf(this.movie.id) > -1
     }
   },
   methods: {
     onSelected() {
-      this.selected = true
-      this.$emit('on-selected-movie', this.movie)
-    }
+      this.$emit('on-selected-movie', this.movie, !this.isSelected)
+    },
   }
 }
 </script>
--- src/pages/AppMovies.vue
+++ src/pages/AppMovies.vue
@@ -2,14 +2,61 @@
   <div>
     <movie-search @search-term="onSearchTermChanged"/>
     <div class="ml-2 mt-4">List of movies</div>
-    <div class="ml-2 mt-4">Selected: {{ selectedMoviesCounter }}</div>
+
+    <b-badge class="ml-2 mt-4">
+      Selected: {{ selectedMoviesCounter }}
+    </b-badge>
+    <b-button
+      variant="primary"
+      class="btn-sm"
+      @click="selectAll"
+    >
+      Select all
+    </b-button>
+    <b-button
+        variant="primary"
+        class="btn-sm"
+        @click="deselectMovies"
+      >
+        Deselect all
+    </b-button>
+    <b-dropdown text="Sort by">
+      <b-dropdown-item
+        @click="sortBy('title', 'asc')"
+      >
+        Name Asc
+      </b-dropdown-item>
+      <b-dropdown-item
+        @click="sortBy('title', 'desc')"
+      >
+        Name Desc
+      </b-dropdown-item>
+      <b-dropdown-divider></b-dropdown-divider>
+      <b-dropdown-item
+        @click="sortBy('duration', 'asc')"
+      >
+        Duration Asc
+      </b-dropdown-item>
+      <b-dropdown-item
+        @click="sortBy('duration', 'desc')"
+      >
+        Duration Desc
+      </b-dropdown-item>
+    </b-dropdown>
     <div class="container">
       <movie-row
-        v-for="movie in movies"
+        v-for="movie in filteredMovies"
         :key="movie.id"
         :movie="movie"
+        :selectedMoviesIds="selectedMoviesIds"
         @on-selected-movie="onSelectedMovie" />
 
+        <movies-paginator
+          :pages="pages"
+          v-if="pages > 1"
+          @selected-page="onSelectedPage"
+          :selectedPage="selectedPage"
+        />
         <div v-if="!movies.length">
           No movies
         </div>
@@ -21,22 +68,29 @@
 import { moviesService } from '../utils/MoviesService'
 import MovieRow from '../components/MovieRow'
 import MovieSearch from '../components/MovieSearch'
+import MoviesPaginator from '../components/MoviesPaginator'
 
 export default {
   components: {
     MovieRow,
-    MovieSearch
+    MovieSearch,
+    MoviesPaginator
   },
   data() {
     return {
       movies: [],
-      selectedMovies: []
+      selectedMoviesIds: [],
+      selectedPage: 1,
+      currentTerm: ''
     }
   },
   beforeRouteEnter(to, from, next) {
     moviesService.getAllMovies().then(({ data }) => {
       next((context) => {
-        context.movies = data
+        context.movies = data.map((movie => {
+          movie.duration = parseInt(movie.duration)
+          return movie
+        }))
       })
     }).catch((error) => {
       console.error(error)
@@ -44,22 +98,57 @@ export default {
   },
   methods: {
     onSearchTermChanged(term) {
-      moviesService.getAllMovies(term).then((response) => {
-        this.movies = response
-      }).catch((error) => {
-        console.error(error)
-      })
+      // moviesService.getAllMovies(term).then((response) => {
+      //   this.movies = response
+      // }).catch((error) => {
+      //   console.error(error)
+      // })
+      this.currentTerm = term
     },
-    onSelectedMovie(movie) {
-      if (this.selectedMovies.indexOf(movie) > -1) {
+    onSelectedMovie(movie, isSelected) {
+      if (!isSelected) {
+        let movieIndex = this.selectedMoviesIds.indexOf(movie.id)
+        this.selectedMoviesIds.splice(movieIndex, 1)
         return;
       }
-      this.selectedMovies.push(movie)
+      this.selectedMoviesIds.push(movie.id)
+    },
+    deselectMovies() {
+      this.selectedMoviesIds = []
+    },
+    selectAll() {
+      this.selectedMoviesIds = this.movies.map(movie => movie.id)
+    },
+    sortBy(prop, order) {
+      let orderCoefficient = order === 'asc' ? 1 : -1
+
+      this.movies = this.movies.sort((movie1, movie2) => {
+        return movie1[prop] >= movie2[prop] ? orderCoefficient : -orderCoefficient
+      })
+    },
+    onSelectedPage(page) {
+      this.selectedPage = page
     }
   },
   computed: {
     selectedMoviesCounter() {
-      return this.selectedMovies.length
+      return this.selectedMoviesIds.length
+    },
+    pages() {
+      return Math.ceil(this.movies.length / 5)
+    },
+    currentlyVisibleMovies() {
+      let bottomLimit = (this.selectedPage -1) * 5
+      let topLimit =  bottomLimit + 5
+
+      return this.movies.filter((movie, index) => {
+        return index >= bottomLimit && index < topLimit
+      })
+    },
+    filteredMovies() {
+      return this.currentlyVisibleMovies.filter((movie) => {
+        return movie.title.toLowerCase().includes(this.currentTerm.toLowerCase())
+      })
     }
   }
 }
